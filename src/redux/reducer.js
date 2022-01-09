1eb3f6d34791636ea00f859167a3a9e8ee52bd27
RomanKhmelenko <xmelenko1992@gmail.com>
2022-01-09 18:51:56 +0200

    reducer corrected

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -9,11 +9,11 @@ const defaultState = {
 const reducer = (state = defaultState, action) => {
   switch(action.type) {
     case ADD_USERS:
-      return {...state, users: [...state.users, ...action.payload]}
+        return {...state, users: action.payload}
     case ADD_POSTS:
-        return {...state, posts: [...state.posts, ...action.payload]}
+        return {...state, posts: action.payload}
     case ADD_COMMENTS:
-        return {...state, comments: [...state.comments, ...action.payload]}
+        return {...state, comments: action.payload}
     default:
       return state
   }
